2598a6312cf539ca562c29e71eae7786745c2021
Flamingo-programmer <86733589+Flamingo-programmer@users.noreply.github.com>
2023-07-25 00:59:57 +0400

    image

--- src/character/Player.java
+++ src/character/Player.java
@@ -1,8 +1,8 @@
 package character;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -10,7 +10,7 @@ import javax.imageio.ImageIO;
 import main.GamePanel;
 import main.InputHandler;
 
-public class Player extends Character{
+public class Player extends Character {
     GamePanel gp;
     InputHandler inputH;
 
@@ -33,15 +33,23 @@ public class Player extends Character{
 
         try {
 
-            up1 = ImageIO.read(getClass().getResourceAsStream("path"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("path"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("path"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("path"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("path"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("path"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("path"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("path"));
-            
+            up1 = ImageIO.read(
+                    new File("/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/up1.png"));
+            up2 = ImageIO.read(
+                    new File("/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/up2.png"));
+            down1 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/down1.png"));
+            down2 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/down2.png"));
+            right1 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/right1.png"));
+            right2 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/right2.png"));
+            left1 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/left1.png"));
+            left2 = ImageIO.read(new File(
+                    "/Users/nielsenaurmoogum/Documents/University/OOP/Project/RPG/resources/player/left2.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,42 +57,43 @@ public class Player extends Character{
 
     public void update() {
 
-        if (inputH.upInput == true || inputH.downInput == true || inputH.leftInput == true || inputH.rightInput == true) {
+        if (inputH.upInput == true || inputH.downInput == true || inputH.leftInput == true
+                || inputH.rightInput == true) {
 
             if (inputH.upInput == true) {
                 direction = "up";
                 y -= speed;
             }
-    
+
             else if (inputH.downInput == true) {
                 direction = "down";
                 y += speed;
             }
-    
+
             else if (inputH.leftInput == true) {
                 direction = "left";
                 x -= speed;
             }
-    
+
             else if (inputH.rightInput == true) {
                 direction = "right";
                 x += speed;
             }
-    
+
             spriteCounter++;
             if (spriteCounter > 15) {
 
                 if (spriteNum == 1) {
                     spriteNum = 2;
                 }
-    
+
                 else if (spriteNum == 2) {
                     spriteNum = 1;
                 }
             }
         }
     }
-    
+
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
 
@@ -92,32 +101,28 @@ public class Player extends Character{
             case "up":
                 if (spriteNum == 1) {
                     image = up1;
-                }
-                else if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = up2;
                 }
                 break;
             case "down":
                 if (spriteNum == 1) {
                     image = down1;
-                }
-                else if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = down2;
                 }
                 break;
             case "left":
                 if (spriteNum == 1) {
                     image = left1;
-                }
-                else if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = left2;
                 }
                 break;
             case "right":
                 if (spriteNum == 1) {
                     image = right1;
-                }
-                else if (spriteNum == 2) {
+                } else if (spriteNum == 2) {
                     image = right2;
                 }
                 break;
