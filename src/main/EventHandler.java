1135300c1c5ea0f17a4b515cabe7e95690ccb51f
ghennahpoyroo <ghennah.poyroo@umail.uom.ac.mu>
2023-08-05 22:13:58 +0400

    event2

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -1,51 +1,76 @@
 package main;
 
-import java.awt.Rectangle;
-
 public class EventHandler {
 
     GamePanel gp;
-    Rectangle eventRect;
+    EventRect eventRect[][];
     int eventRectDefaultX, eventRectDefaultY;
+
+    int previousEventX, previousEventY;
+    boolean canTouchEvent;
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+
+        int col = 0;
+        int row = 0;
+        while (col < gp.maxWorldCol && row < gp.maxScreenRow) {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+        }
+
     }
 
     // What kind of event and where are the events
     // on the map
     public void checkEvent() {
-        if (hit(4, 4, "left") == true) {
-            fire(gp.dialogueState);
+
+        // check if player is more than one tile away from previous event
+        int xDistance = Math.abs(gp.player.worldX = previousEventX);
+        int yDistance = Math.abs(gp.player.worldY = previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+
+        if (distance > gp.tileSize) {
+            canTouchEvent = true;
         }
+
+        if (canTouchEvent = true) {
+            if (hit(4, 4, "left") == true) {
+                fire(gp.dialogueState);
+            }
+        }
+
     }
 
     // Check for event collision
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             if (gp.player.direction.contains(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
+
+                previousEventX = gp.player.worldX;
+                previousEventY = gp.player.worldY;
             }
         }
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
@@ -55,6 +80,7 @@ public class EventHandler {
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You are on fire but in a bad way";
         gp.player.life -= 1;
+
     }
 
 }
